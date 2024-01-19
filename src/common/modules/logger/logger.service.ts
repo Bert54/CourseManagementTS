1db87262006762b6a8374f9a20603182e753b041
Matthias <matt.bert@live.fr>
2024-01-19 18:06:22 +0100

    Adjust logger

--- src/common/modules/logger/logger.service.ts
+++ src/common/modules/logger/logger.service.ts
@@ -2,6 +2,9 @@ import { ConsoleLogger, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class LoggerService extends ConsoleLogger {
+  constructor() {
+    super();
+  }
   error(message: any, stack?: string, context?: string) {
     super.error(message, stack, context);
   }
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import * as Config from 'config';
 import { AppConfig } from './config';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggerService } from './common';
 
 async function bootstrap(config: AppConfig) {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new LoggerService(),
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
