ce6b2b0645142fd4211b81b7d2815c5620ea5a5b
Matthias <matt.bert@live.fr>
2024-02-05 18:11:07 +0100

    Improve global error logging

--- src/common/interceptors/error-logger.interceptor.ts
+++ src/common/interceptors/error-logger.interceptor.ts
@@ -14,21 +14,26 @@ import { LoggerService } from '../modules/logger';
 export class ErrorLoggerInterceptor implements NestInterceptor {
   constructor(private logger: LoggerService) {}
   intercept(
-    context: ExecutionContext,
-    next: CallHandler<any>,
+    _: ExecutionContext,
+    next: CallHandler,
   ): Observable<any> | Promise<Observable<any>> {
     return next.handle().pipe(
       tap({
         error: (error) => {
           if (!(error instanceof HttpException)) {
-            this.logger.log(
+            // not a http error, should always be logged as warn
+            this.logger.warn(
               `An error occurred while performing the request [error: '${error.message}']`,
             );
           } else {
             const httpErr = error as HttpException;
-            this.logger.log(
-              `An error occurred while performing the request [status: ${httpErr.getStatus()} ; cause: '${httpErr.cause}']`,
-            );
+            const message = `An error occurred while performing the request [status: ${httpErr.getStatus()} ; cause: '${httpErr.cause}']`;
+            // log as warn if http error is in the 500 range, otherwise just log as info
+            if (httpErr.getStatus() >= 500 && httpErr.getStatus() < 600) {
+              this.logger.warn(message);
+            } else {
+              this.logger.log(message);
+            }
           }
         },
       }),
